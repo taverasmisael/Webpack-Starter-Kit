f905c6cd34a726336d25810a82855df4e2ae615e
taverasmisael <macrot29@gmail.com>
2018-02-25 13:32:05 -0400

    refact(dynamic import): try dynamic imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-const User = { name: 'User' }
-const Hello = document.createElement('P')
-Hello.innerText = `Hi ${User.name}`
-document.querySelector('body').appendChild(Hello)
+import(/* webpackChunkName: "userdata" */ './userdata').then(({ default: User }) => {
+  const Hello = document.createElement('P')
+  Hello.innerText = `Hi ${User.name}`
+  document.querySelector('body').appendChild(Hello)
+})
