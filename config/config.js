19e0b2851cc704d814004a54685ef51821f957b9
taverasmisael <macrot29@gmail.com>
2018-02-28 01:51:06 -0400

    fix(config): fix exports in config file

--- config/config.js
+++ config/config.js
@@ -3,16 +3,8 @@ import { ProjectConfig, name } from '../package.json'
 
 const replaceUnderscores = (string = '') => [...string].map(s => (s === '_' ? ' ' : s)).join('')
 
-const BASE_FOLDER = resolve(__dirname, '../')
-const SOURCE_FOLDER = resolve(BASE_FOLDER, ProjectConfig.source)
-const DIST_FOLDER = resolve(BASE_FOLDER, ProjectConfig.dist)
-const APP_NAME = ProjectConfig.displayName
-const PACKAGE_NAME = replaceUnderscores(name || APP_NAME)
-
-export default {
-  BASE_FOLDER,
-  SOURCE_FOLDER,
-  DIST_FOLDER,
-  PACKAGE_NAME,
-  APP_NAME
-}
+export const BASE_FOLDER = resolve(__dirname, '../')
+export const SOURCE_FOLDER = resolve(BASE_FOLDER, ProjectConfig.source)
+export const DIST_FOLDER = resolve(BASE_FOLDER, ProjectConfig.dist)
+export const APP_NAME = ProjectConfig.displayName
+export const PACKAGE_NAME = replaceUnderscores(name || APP_NAME)
