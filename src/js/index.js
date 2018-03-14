6403918d3f15a0983e1d3b893f131164bd91bc8a
taverasmisael <macrot29@gmail.com>
2018-03-14 19:22:03 -0400

    refact(jsfiles): remove unneded/demo code

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,1 @@
 // @flow
-import(/* webpackChunkName: "userdata" */ './userdata').then(({ default: User }) => {
-  const Hello = document.createElement('P')
-  Hello.innerText = `Hi ${new User().name} ${User.method()}`
-  const body = document.querySelector('body')
-  if (body) body.appendChild(Hello)
-})
